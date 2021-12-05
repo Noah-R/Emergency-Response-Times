e10b860da346c555dc2c9342f6a2704cf6978b79
Noah Rutkovsky <noah.rutkovsky@gmail.com>
2021-12-04 22:02:48 -0500

    that didn't take that long

--- mapping.py
+++ mapping.py
@@ -1,15 +1,41 @@
 import folium
 import pandas as pd
 
+def processdata(filename):
+    rawdata = pd.read_csv(filename, dtype={'ZIPCODE': str}).dropna()
+    rawdata["ZIPCODE"] = rawdata["ZIPCODE"].apply(lambda x: x.replace(",",""))
+    rawdata = rawdata.astype({'ZIPCODE': 'int32'})
+
+    zipcodes = list(rawdata["ZIPCODE"].unique())
+    averages = []
+
+    for code in zipcodes:
+        col = rawdata[rawdata["ZIPCODE"] == code]["INCIDENT_TRAVEL_TM_SECONDS_QY"]
+        averages.append(col.mean())
+
+    for i in range(len(zipcodes)):
+        if(zipcodes[i]==83):
+            zipcodes[i]="00083"
+        else:
+            zipcodes[i]=str(zipcodes[i])
+
+    processeddata = pd.DataFrame()
+    processeddata["zipcode"] = zipcodes
+    processeddata["average_response_time"] = averages
+
+    processeddata.to_csv("processeddata.csv", index=False)
+
+#processdata("responsetimes.csv")
+processeddata = pd.read_csv("processeddata.csv", dtype={'zipcode': str})
+
 map = folium.Map(location=[40.7128, -74.0060], zoom_start=12)
-data = pd.read_csv("responsetimes.csv")
 
 folium.Choropleth(
     geo_data="zipcodes.json",
-    data=data,
-    columns=["ZIPCODE", "INCIDENT_TRAVEL_TM_SECONDS_QY"],
+    data=processeddata,
+    columns=["zipcode", "average_response_time"],
     key_on="feature.properties.postalCode",
-    fill_color="YlGn",
+    fill_color="YlOrRd",
     fill_opacity=0.7,
     line_opacity=0.2,
     legend_name="EMS Average Response Time(Seconds)",
